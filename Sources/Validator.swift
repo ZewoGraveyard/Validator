161becbcfb1bf9601b1ba926e133f5f9659efae4
Oleg Dreyman <dreymonde@me.com>
2016-04-29 22:24:29 +0300

    Sequence extension

--- Sources/Validator.swift
+++ Sources/Validator.swift
@@ -17,3 +17,9 @@ public enum ValidationError: ErrorProtocol {
     case validationFailed
     case incompatibleType
 }
+
+extension Sequence where Iterator.Element == Validator {
+    public func validate(_ value: Any) throws {
+        try self.forEach({ try $0.validate(value) })
+    }
+}
